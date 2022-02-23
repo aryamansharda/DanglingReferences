3d09688ebec2b3f797f3fede3247b7674ce988f5
aryamansharda <asharda@turo.com>
2022-02-23 09:35:31 -0800

    Improvements to ObjC support.

--- FindDanglingReferences.py
+++ FindDanglingReferences.py
@@ -14,8 +14,8 @@ def findAllFilesWithExtension(path, extension):
 
 	for r, d, f in os.walk(path):
 	    for file in f:
-	        if extension in file:
-	            files.append(os.path.join(r, file))
+	    	if extension in file:
+	    		files.append(os.path.join(r, file))
 	return files
 
 def extract_ib_outlets_from_table_view_cell(view_controller):
@@ -48,32 +48,9 @@ def load_ib_outlets_from_storyboards():
 	storyboard_ib_outlet_map = {}	
 	table_view_cell_to_ib_outlet_map = {}
 	collection_view_cell_to_ib_outlet_map = {}
-	xib_to_ib_outlet_map = {}
 
 	# Split into .storyboard and .swift files
 	storyboards = findAllFilesWithExtension("/Users/aryamansharda/Documents/bowman/Bowman", ".storyboard")
-	xibs = findAllFilesWithExtension("/Users/aryamansharda/Documents/bowman/Bowman", ".xib")
-
-	for xib in xibs:
-		print("XIB: " + xib)
-		tree = ET.parse(xib)
-		root = tree.getroot()
-
-		files_owner = root.findall('.//placeholder')
-		for file_owner in files_owner:
-			print(file_owner.attrib)
-			if "customClass" in file_owner.attrib:
-				custom_xib_class_name = file_owner.attrib["customClass"]
-				print(custom_xib_class_name)
-
-				connections = file_owner.findall('./connections/')
-				xib_ib_outlets = [connection.attrib["property"] for connection in connections if connection.tag == "outlet" or connection.tag == "outletCollection"]
-				print(xib_ib_outlets)
-				xib_to_ib_outlet_map[custom_xib_class_name] = set(xib_ib_outlets)
-
-				table_view_cell_to_ib_outlet_map.update(extract_ib_outlets_from_table_view_cell(root))
-				collection_view_cell_to_ib_outlet_map.update(extract_ib_outlets_from_collection_view_cell(root))
-
 
 	for storyboard in storyboards:
 		tree = ET.parse(storyboard)
@@ -93,43 +70,92 @@ def load_ib_outlets_from_storyboards():
 				table_view_cell_to_ib_outlet_map.update(extract_ib_outlets_from_table_view_cell(viewController))
 				collection_view_cell_to_ib_outlet_map.update(extract_ib_outlets_from_collection_view_cell(viewController))
 										
-	return storyboard_ib_outlet_map, table_view_cell_to_ib_outlet_map, collection_view_cell_to_ib_outlet_map, xib_to_ib_outlet_map
+	return storyboard_ib_outlet_map, table_view_cell_to_ib_outlet_map, collection_view_cell_to_ib_outlet_map
+
+def load_ib_outlets_from_objective_c_source():
+	objc_files = findAllFilesWithExtension("/Users/aryamansharda/Documents/bowman/Bowman", ".h") + findAllFilesWithExtension("/Users/aryamansharda/Documents/bowman/Bowman", ".m")
+
+	source_to_ib_outlet_mapping = {}
+	subclass_to_parent_mapping = {}
+
+	for source in objc_files:
+		# Retrieves just the view controller's name
+		source_file_name = Path(source).stem
+		# print(source_file_name)
+
+		with open(source, 'r') as file:
+
+			try:	
+				fileContents = file.read()			
+
+				allIBOutlets = re.findall('\\sIBOutlet.*\\*\\s*(.*);', fileContents)
+				# print(allIBOutlets)
+					
+				if source_file_name in source_to_ib_outlet_mapping:
+					source_to_ib_outlet_mapping[source_file_name] += allIBOutlets
+				else:
+					source_to_ib_outlet_mapping[source_file_name] = allIBOutlets
+
+				# Create a relationship between all of the base and derived classes as this may be needed later on			
+				parentClasses = []
+
+				# print(Path(source).suffix)
+				if ".h" in source:
+					parentClasses = re.findall('@interface\\s+\\w+\\s+:\\s+(\\w+)', fileContents)
+
+					print("Finding parents")			
+					print(parentClasses)
+					
+					if parentClasses:
+						subclass_to_parent_mapping[source_file_name] = parentClasses[0].split(' ')[-1]
+						# print("Parents: " + str(parentClasses[0].split(' ')[-1]))
+			except:
+				print("Failed to process file: " + source_file_name)
+
+	return source_to_ib_outlet_mapping, subclass_to_parent_mapping
 
 def load_ib_outlets_from_swift_source():
 	# Find a matching .swift file and extract the IBOutlets
 	swiftSourceFiles = findAllFilesWithExtension("/Users/aryamansharda/Documents/bowman/Bowman", ".swift")
 
 	source_to_ib_outlet_mapping = {}
-	subclass_to_parent_mapping = {}
+	swift_subclass_to_parent_mapping = {}
 
 	for source in swiftSourceFiles:
 		# Retrieves just the view controller's name
 		source_file_name = Path(source).stem
 		with open(source, 'r') as file:
 
-			fileContents = file.read()			
+			fileContents = file.read()
+			print(source_file_name)
 
 			# Gets all IBOutlets with optionality \\s@IBOutlet.*.var\\s(.*):.*!
 			allIBOutlets = re.findall('\\s@IBOutlet.*.var\\s(.*):', fileContents)
+			print(allIBOutlets)
 			parentClasses = re.findall(source_file_name + ':.(.*.){', fileContents)
 
 			source_to_ib_outlet_mapping[source_file_name] = set(allIBOutlets)
 
 			# Create a relationship between all of the base and derived classes as this may be needed later on
 			if parentClasses:
-				subclass_to_parent_mapping[source_file_name] = parentClasses[0].split(',')
+				swift_subclass_to_parent_mapping[source_file_name] = parentClasses[0].split(',')
 
-	return source_to_ib_outlet_mapping, subclass_to_parent_mapping
+	return source_to_ib_outlet_mapping, swift_subclass_to_parent_mapping
 
 def find_ib_outlets_in_parent_class(swift_source_ib_outlet_map, subclass_to_parent_mapping, current_child_view_controller):
 	parents_ib_outlets = []
-	parent_classes = subclass_to_parent_mapping[current_child_view_controller]
 
-	for potential_parent in parent_classes:
-		potential_parent = potential_parent.strip()
+	try:
+		parent_classes = subclass_to_parent_mapping[current_child_view_controller]
 
-		if potential_parent in swift_source_ib_outlet_map:
-			parents_ib_outlets += swift_source_ib_outlet_map[potential_parent]
+		for potential_parent in parent_classes:
+			potential_parent = potential_parent.strip()
+
+			if potential_parent in swift_source_ib_outlet_map:
+				parents_ib_outlets += swift_source_ib_outlet_map[potential_parent]
+	except:
+		print("")
+		# Key error on: " + current_child_view_controller)
 	
 	return set(parents_ib_outlets)
 
@@ -138,12 +164,15 @@ def validate_ib_outlet_connections(ib_outlet_map):
 	for key, value in ib_outlet_map.items():
 		logger.info("Processing " + key + "...")
 
-		if key in swift_source_ib_outlet_map:
-			outlets_defined_in_parent_class = set(find_ib_outlets_in_parent_class(swift_source_ib_outlet_map, subclass_to_parent_mapping, key))
-			outlets_defined_in_code = swift_source_ib_outlet_map[key]
+		if key in all_ib_outlet_map:
+			outlets_defined_in_parent_class = set(find_ib_outlets_in_parent_class(all_ib_outlet_map, subclass_to_parent_mapping, key))
+			outlets_defined_in_code = set(all_ib_outlet_map[key])
 			outlets_from_storyboard = value
 
 			result = set()
+
+			# print(outlets_defined_in_code)
+			# print(outlets_from_storyboard)
 
 			if len(outlets_defined_in_code) >= len(outlets_from_storyboard):
 				result = outlets_defined_in_code - outlets_from_storyboard
@@ -171,10 +200,22 @@ def validate_ib_outlet_connections(ib_outlet_map):
 
 	print("Failures: " + str(failures))
 
-view_controllers_ib_outlet_map, table_view_cell_to_ib_outlet_map, collection_view_cell_to_ib_outlet_map, xib_to_ib_outlet_map = load_ib_outlets_from_storyboards()
-swift_source_ib_outlet_map, subclass_to_parent_mapping = load_ib_outlets_from_swift_source()
+objc_source_ib_outlet_map, objc_subclass_to_parent_mapping = load_ib_outlets_from_objective_c_source()
+view_controllers_ib_outlet_map, table_view_cell_to_ib_outlet_map, collection_view_cell_to_ib_outlet_map = load_ib_outlets_from_storyboards()
+swift_source_ib_outlet_map, swift_subclass_to_parent_mapping = load_ib_outlets_from_swift_source()
 
-# validate_ib_outlet_connections(view_controllers_ib_outlet_map)
+print("Output:")
+print(swift_source_ib_outlet_map)
+
+# Combine ObjC & Swift child parent relationships into one dictionary
+subclass_to_parent_mapping = {}
+subclass_to_parent_mapping = objc_subclass_to_parent_mapping.copy()
+subclass_to_parent_mapping.update(swift_subclass_to_parent_mapping)
+
+# Combine ObjC & Swift IBOutlet declarations into one dictionary
+all_ib_outlet_map = objc_source_ib_outlet_map.copy()
+all_ib_outlet_map.update(swift_source_ib_outlet_map)
+
+validate_ib_outlet_connections(view_controllers_ib_outlet_map)
 validate_ib_outlet_connections(table_view_cell_to_ib_outlet_map)
-# validate_ib_outlet_connections(collection_view_cell_to_ib_outlet_map)
-validate_ib_outlet_connections(xib_to_ib_outlet_map)
+validate_ib_outlet_connections(collection_view_cell_to_ib_outlet_map)
